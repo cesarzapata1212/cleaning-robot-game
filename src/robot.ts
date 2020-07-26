7d2910106118646199f228b153e55d626580a97e
Cesar Zapata - Amido <cesarzapataamido@AML0209.local>
2020-07-26 12:56:22 +0100

    Adds moving

--- src/robot.ts
+++ src/robot.ts
@@ -1,30 +1,98 @@
 
 export class Robot {
-	constructor() {
-	}
 
-	onObstacleDetected(): void {
-	}
+    private _position: Position
+    private _direction: Angle
+    private _speed: number;
 
-	changeDirection(): void {
-	}
+    private readonly SPEED_STOPPED = 0;
+    private readonly SPEED_DEFAULT = 5;
 
-	move(): void {
-	}
+    constructor(position?: Position, direction?: Angle) {
+        this._position = position || new Position(0, 0)
+        this._direction = direction || new Angle(0)
+        this._speed = this.SPEED_STOPPED
+    }
 
-	isStuck(): boolean {
-		return false
-	}
+    onObstacleDetected(): void {
+    }
 
-	get speedX(): number {
-        return 0;
-	}
+    changeDirection(): void {
+    }
 
-	get x(): number {
-		return 0
-	}
+    move(): void {
+        this._speed = this.SPEED_DEFAULT
+        let rotation = this._direction.degrees() * Math.PI / 180
+        let vx = this._position.x + this._speed * Math.cos(rotation)
+        let vy = this._position.x + this._speed * Math.sin(rotation)
+        this._position = new Position(vx, vy)
+    }
 
-	get y(): number {
-		return 0
-	}
+    isStuck(): boolean {
+        return false
+    }
+
+    get speed(): number {
+        return this._speed
+    }
+
+    get x(): number {
+        return this._position.x
+    }
+
+    get y(): number {
+        return this._position.y
+    }
+
+    get direction(): Angle {
+        return this._direction
+    }
+}
+
+export class Position {
+
+    private _x: number
+    private _y: number
+
+    constructor(x: number, y: number) {
+        this._x = x
+        this._y = y
+    }
+
+    get x(): number {
+        return this._x
+    }
+
+    get y(): number {
+        return this._y
+    }
+}
+
+export class Angle {
+
+    public static FORWARD: Angle = new Angle(0)
+
+    private _value: number
+
+    private readonly MIN: number = -360
+    private readonly MAX: number = 360
+
+    constructor(value?: number) {
+        this._value = value || 0
+
+        if (this._value < this.MIN || this._value > this.MAX) {
+            throw new AngleValueError(this._value)
+        }
+    }
+
+    degrees(): number {
+        return this._value
+    }
+}
+
+export class AngleValueError extends Error {
+
+    constructor(value: number) {
+        super(`Invalid angle provided ${value.toString()}`)
+    }
 }
--- test/robot.spec.ts
+++ test/robot.spec.ts
@@ -1,13 +1,94 @@
-import "jest"
+import 'jest'
 
-import { Robot } from 'app/Robot'
+import { Robot, Position, Angle, AngleValueError } from 'app/robot'
 
-describe("Robot", () => {
-    test("should be defined", () => {
-        expect(Robot).toBeDefined();
+
+describe('robot module', () => {
+    describe('Robot', () => {
+        test('should be defined', () => {
+            expect(Robot).toBeDefined();
+        })
+
+        test('should set defaults', () => {
+            let robot = new Robot()
+
+            expect(robot.x).toEqual(0)
+            expect(robot.y).toEqual(0)
+            expect(robot.speed).toEqual(0)
+            expect(robot.direction).toEqual(new Angle(0))
+        });
+
+        test('constructor should take initial position', () => {
+            let position = new Position(1, 1)
+            let direction = new Angle(90)
+
+            let robot = new Robot(position, direction)
+
+            expect(robot.x).toEqual(position.x)
+            expect(robot.y).toEqual(position.y)
+            expect(robot.direction).toEqual(new Angle(90))
+        })
+
+        test('should increase speed when move is called', () => {
+            let robot = new Robot()
+            expect(robot.speed).toEqual(0)
+
+            robot.move()
+
+            expect(robot.speed).toEqual(5)
+        });
+
+        describe('move', () => {
+            test('should change pivot X when moving forward (Angle=0)', () => {
+                let robot = new Robot(new Position(0, 0), Angle.FORWARD)
+
+                robot.move()
+
+                expect(robot.x).toEqual(5)
+                expect(robot.y).toEqual(0)
+            });
+
+            test('should change pivot Y when moving at 90 degrees angle', () => {
+                let robot = new Robot(new Position(0, 0), new Angle(90))
+
+                robot.move()
+
+                expect(robot.y).toEqual(5)
+            });
+        });
     })
 
-    test("", () => {
+    describe('Position', () => {
+        test('should be defined', () => {
+            expect(Position).toBeDefined()
+        })
 
+        test('should take initial position', () => {
+            expect(new Position(1, 0).x).toEqual(1)
+            expect(new Position(1, 0).y).toEqual(0)
+
+            expect(new Position(0, 1).x).toEqual(0)
+            expect(new Position(0, 1).y).toEqual(1)
+        })
+    })
+
+    describe('Angle', () => {
+        test('should be defined', () => {
+            expect(Angle).toBeDefined()
+        })
+
+        test('should have angle 0 by default', () => {
+            expect(new Angle().degrees()).toEqual(0)
+        });
+
+        test('should accept only range from -360 to 360 degree', () => {
+            expect(new Angle(-359).degrees()).toEqual(-359)
+            expect(new Angle(-360).degrees()).toEqual(-360)
+            expect(() => new Angle(-361)).toThrow(new AngleValueError(-361))
+
+            expect(new Angle(359).degrees()).toEqual(359)
+            expect(new Angle(360).degrees()).toEqual(360)
+            expect(() => new Angle(361)).toThrow(new AngleValueError(361))
+        });
     })
 })
